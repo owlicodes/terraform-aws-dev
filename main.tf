aa2b99dbd3b6231b502b1e9e2751020b36f0422f
Jose Diago <owlicodes@gmail.com>
2024-11-04 14:36:44 +0800

    Create security group.

--- main.tf
+++ main.tf
@@ -45,3 +45,23 @@ resource "aws_route_table_association" "mtc_public_route_association" {
   subnet_id      = aws_subnet.mtc_public_subnet.id
   route_table_id = aws_route_table.mtc_public_route_table.id
 }
+
+resource "aws_security_group" "mtc_security_group" {
+  name        = "dev-security-group"
+  description = "Security group for development environment."
+  vpc_id      = aws_vpc.mtc_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
